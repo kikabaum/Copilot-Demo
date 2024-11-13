dbeded25478c4980d8062d47a3ac55846116a4ce
nisenchris <nisenchris@gmail.com>
2024-11-12 23:52:50 -0600

    remove obsolete test files for SalesTable, helloWorld, SalesOverview, and Dashboard components

--- src/__tests__/helloWorld.test.js
+++
@@ -1,7 +0,0 @@
-const helloWorld = require('../helloWorld');
-
-describe('helloWorld function', () => {
-    test('returns "Hello, World!"', () => {
-        expect(helloWorld()).toBe('Hello, World!');
-    });
-});
